4d1787d1122149af3601c987c6ea278a11f231a4
Refaat0 <elhajj.refaat@gmail.com>
2023-02-06 11:56:05 -0700

    setup

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
-import "./lib/db";
+import mongoose from "mongoose";
 import express from "express";
-import countryRoutes from "./routes/country";
 
 const app = express();
 const port = process.env.PORT || 3333;
+
+mongoose.connect('mongodb+srv://admin:admin@cluster0.neilvdo.mongodb.net/?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use(express.json());
 app.use(express.raw({ type: "application/vnd.custom-type" }));
 app.use(express.text({ type: "text/html" }));
 
 app.get("/", async (req, res) => {
-  res.json({ message: "Please visit /countries to view all the countries" });
+  res.json({ message: "hello, world!" });
 });
 
-app.use("/countries", countryRoutes);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
